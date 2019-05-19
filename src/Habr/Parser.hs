8669a6295bf90b7ce6e54e43172428016adb411a
0xd34df00d <0xd34df00d@gmail.com>
2019-05-19 15:42:59 -0400

    Stub parsing

---
+++ src/Habr/Parser.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE RecordWildCards, OverloadedStrings, ApplicativeDo, QuasiQuotes #-}
+
+module Habr.Parser where
+
+import qualified Data.Text as T
+import qualified Data.Text.Read as T
+import Control.Monad
+import Text.XML
+import Text.XML.Cursor
+import Text.XML.Scraping
+import Text.XML.Selector.TH
+import Text.XML.Selector.Types
+
+import Habr.Types
+
+parseComments :: Cursor -> Either String [Comment]
+parseComments = undefined
+
+(@@) :: Cursor -> Name -> Either String T.Text
+cur @@ name | [val] <- attrs = pure val
+            | otherwise = Left $ "unexpected attribute contents for " <> show name <> ": " <> show attrs
+  where attrs = attribute name cur
+
+(@@^) :: Either String Cursor -> Name -> Either String T.Text
+mcur @@^ name = mcur >>= (@@ name)
+
+(@>) :: Cursor -> [JQSelector] -> Either String Cursor
+cur @> expr | (sub:_) <- queryT expr cur = pure sub
+            | otherwise = Left $ "nothing found for expression " <> show expr
+
+readInt :: T.Text -> Either String Int
+readInt text = do
+  (val, rest) <- T.decimal text
+  guard $ not $ T.null rest
+  pure val
+
+parseSingleComment :: Cursor -> Either String Comment
+parseSingleComment cur = do
+  parentId <- cur @> [jq| span.parent_id |] @@^ "data-parent_id" >>= readInt
+  commentId <- cur @@ "rel" >>= readInt
+  commentText <- innerHtml <$> (cur @> [jq| div.comment__message |])
+  let children = []
+  pure Comment { .. }
