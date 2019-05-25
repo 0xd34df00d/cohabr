c1d9211fbab92fc3894bae698e7974b8c16ef3ac
0xd34df00d <0xd34df00d@gmail.com>
2019-05-25 17:18:29 -0400

    Comment user parser

--- src/Habr/Parser.hs
+++ src/Habr/Parser.hs
@@ -5,6 +5,8 @@ module Habr.Parser where
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Read as T
+import Control.Monad
+import Data.Either.Combinators
 import Data.String.Interpolate
 import Text.XML
 import Text.XML.Cursor
@@ -41,5 +43,15 @@ parseSingleComment cur = do
   parentId <- cur @> [jq| span.parent_id |] @@^ "data-parent_id" >>= readInt
   commentId <- cur @@ "rel" >>= readInt
   commentText <- TL.toStrict . innerHtml <$> (cur @> [jq| div.comment__message |])
+  user <- parseCommentUser cur
   let children = []
   pure Comment { .. }
+
+parseCommentUser :: Cursor -> Either String UserInfo
+parseCommentUser cur = do
+  username <- cur @> [jq| a.user_info |] @@^ "data-user-login"
+  avatar <- maybeToRight "Unable to parse image" $ msum [defaultImg, userImg]
+  pure UserInfo { .. }
+  where
+    defaultImg = DefaultAvatar . TL.toStrict . toHtml <$> rightToMaybe (cur @> [jq|svg|])
+    userImg = CustomAvatar <$> rightToMaybe (cur @> [jq|img.user-info__image-pic|] @@^ "src")
