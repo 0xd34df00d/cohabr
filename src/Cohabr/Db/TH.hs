17ea9cfb16da2f22d34f241b40982197d23380c6
0xd34df00d <0xd34df00d@gmail.com>
2019-06-03 16:48:15 -0400

    Generate function decl as well in makeTFAdaptorAndInstance

--- src/Cohabr/Db/TH.hs
+++ src/Cohabr/Db/TH.hs
@@ -6,11 +6,13 @@ module Cohabr.Db.TH where
 
 import Data.Generics.Uniplate.Data
 import Data.List
-import Data.Tuple.Select
+import qualified Data.Profunctor as P
 import qualified Data.Profunctor.Product as PP
 import qualified Data.Profunctor.Product.Default as D
 import Data.Profunctor.Product.Default(Default)
+import Data.Tuple.Select
 import Language.Haskell.TH
+import Opaleye.TypeFamilies((:<$>), (:<*>))
 
 makeTFAdaptorAndInstance :: String -> Name -> Q [Dec]
 makeTFAdaptorAndInstance funName tyName = reify tyName >>= \case
@@ -24,11 +26,14 @@ handleTyConDec funName (DataD _ tyName [tyVar] _ [con] _) = handleTyConCon funNa
 handleTyConDec _ _ = fail "Type shall have a single constructor and a single type variable"
 
 handleTyConCon :: String -> Name -> Name -> Con -> Q [Dec]
-handleTyConCon funName tyName tyVarName (RecC conName vars) =
-  pure [makeInstanceDec funName tyName tyVarName conName $ sel3 <$> vars]
+handleTyConCon funName tyName tyVarName (RecC conName vars) = pure [ instDec, sigDec, funDec ]
+  where
+    funName' = mkName funName
+    instDec = makeInstanceDec funName' tyName tyVarName conName $ sel3 <$> vars
+    (sigDec, funDec) = makeFunDec funName' tyName conName vars
 handleTyConCon _ _ _ _ = fail "Unsupported constructor type (only non-GADT records are supported)"
 
-makeInstanceDec :: String -> Name -> Name -> Name -> [Type] -> Dec
+makeInstanceDec :: Name -> Name -> Name -> Name -> [Type] -> Dec
 makeInstanceDec funName tyName tyVarName conName vars = InstanceD Nothing instCtx instTy [defDec]
   where
     instCtx = (ConT ''PP.ProductProfunctor `AppT` p) :
@@ -41,8 +46,28 @@ makeInstanceDec funName tyName tyVarName conName vars = InstanceD Nothing instCt
                 (ConT tyName `AppT` a) `AppT`
                 (ConT tyName `AppT` b)
     defDec = FunD (mkName "def") [Clause [] (NormalB defBodyExp) []]
-    defBodyExp = (VarE $ mkName funName) `AppE` conEDefExp
+    defBodyExp = VarE funName `AppE` conEDefExp
     conEDefExp = foldl AppE (ConE conName) $ replicate (length vars) (VarE 'D.def)
     [p, a, b] = VarT . mkName <$> ["p", "a", "b"]
     replaceTyVar var = transformBi $ \case VarT n | n == tyVarName -> var
                                            ty -> ty
+
+makeFunDec :: Sel1 a Name => Name -> Name -> Name -> [a] -> (Dec, Dec)
+makeFunDec funName tyName conName vars = (sigDec, funDec)
+  where
+    sigDec = SigD funName funTy
+    funDec = FunD funName [Clause [VarP funPatVar] (NormalB funExp) []]
+    [p, a, b] = VarT . mkName <$> ["p", "a", "b"]
+    pab = InfixT (InfixT p ''(:<$>) a) ''(:<*>) b
+    funTy = ForallT [] [ConT ''PP.ProductProfunctor `AppT` p] $
+              ArrowT `AppT`
+                (ConT tyName `AppT` pab) `AppT`
+                (p `AppT`
+                  (ConT tyName `AppT` a) `AppT`
+                  (ConT tyName `AppT` b))
+    funPatVar = mkName "row"
+    ops = '(PP.***$) : repeat '(PP.****)
+    args = [ VarE 'P.lmap `AppE` VarE varName `AppE` (VarE varName `AppE` VarE funPatVar)
+           | varName <- sel1 <$> vars
+           ]
+    funExp = foldl (\l (op, r) -> InfixE (Just l) (VarE op) (Just r)) (ConE conName) $ zip ops args
