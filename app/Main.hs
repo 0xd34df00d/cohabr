b5f1a5dfd071b96591c57609c87b368a0cfd36fe
0xd34df00d <0xd34df00d@gmail.com>
2019-06-04 19:27:02 -0400

    Stub post updates and RSS polling

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,49 @@
+{-# LANGUAGE NoMonomorphismRestriction, FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
+import qualified Database.PostgreSQL.Simple as PGS
+
+import Control.Concurrent.Async
+import Control.Exception
+import Control.Monad.Except
+import Control.Monad.Reader
+import Data.String.Interpolate
+import Data.Time.Clock
+import Network.HTTP.Conduit
+import Text.XML.Cursor(fromDocument)
+import Text.HTML.DOM(parseLBS)
+import Time.Repeatedly
+import System.IO
+
+import Habr.Parser
+import Habr.RSS
+
+withConnection :: (PGS.Connection -> IO c) -> IO c
+withConnection = bracket
+  (PGS.connect PGS.defaultConnectInfo { PGS.connectDatabase = "habr" })
+  PGS.close
+
+updatePost :: Int -> IO ()
+updatePost postId = do
+  now <- getCurrentTime
+  postPage <- simpleHttp [i|https://habr.com/ru/post/#{postId}/|]
+  let root = fromDocument $ parseLBS postPage
+  let parseResult = runExcept $ runReaderT ((,) <$> parsePost root <*> parseComments root) ParseContext { currentTime = now }
+  case parseResult of
+    Left err -> hPutStrLn stderr err
+    Right (post, comments) -> pure ()
+  pure ()
+
+pollRSS :: IO ()
+pollRSS = do
+  rss <- simpleHttp "https://habr.com/ru/rss/all/all/?fl=ru%2Cen"
+  let channels = recentArticles rss
+  print channels
+  pure ()
+
 main :: IO ()
-main = undefined
+main = do
+  rssPollHandle <- asyncRepeatedly (1 / 60) pollRSS
+  wait rssPollHandle
