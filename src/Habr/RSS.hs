d5c834ff6f17be51d04be1a93a207249a5c80e08
0xd34df00d <0xd34df00d@gmail.com>
2019-06-04 15:29:06 -0400

    Added posts RSS parsing module

---
+++ src/Habr/RSS.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Habr.RSS
+( recentArticles
+) where
+
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text as T
+import Data.Maybe
+import Text.Feed.Import
+import Text.Feed.Types
+import Text.RSS.Syntax
+
+import Habr.Internal.Util
+
+recentArticles :: BSL.ByteString -> Maybe [Int]
+recentArticles str = extractArticles <$> parseFeedSource str
+
+extractArticles :: Feed -> [Int]
+extractArticles (RSSFeed RSS { .. }) = extractRssChannel rssChannel
+extractArticles (AtomFeed _) = error "Atom feeds are unsupported"
+extractArticles (RSS1Feed _) = error "RSS1 feeds are unsupported"
+extractArticles (XMLFeed _) = []
+
+extractRssChannel :: RSSChannel -> [Int]
+extractRssChannel RSSChannel { .. } = mapMaybe handleItem rssItems
+  where
+    handleItem item = do
+      guid <- rssItemGuid item
+      -- TODO more type safety
+      let (_:postId:_) = reverse $ T.split (== '/') $ rssGuidValue guid
+      let Right val = readInt postId
+      pure val
