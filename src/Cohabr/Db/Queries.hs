2089c016f5ce1e51720faa085dea3f3ccc84830c
0xd34df00d <0xd34df00d@gmail.com>
2019-06-05 14:26:22 -0400

    Added a query to get post by its habr ID

--- src/Cohabr/Db/Queries.hs
+++ src/Cohabr/Db/Queries.hs
@@ -3,14 +3,17 @@
 
 module Cohabr.Db.Queries
 ( selectMissingPosts
+, findPostByHabrId
 ) where
 
 import qualified Database.PostgreSQL.Simple as PGS
 import Control.Arrow
 import Data.List
 import Opaleye as O
+import Opaleye.TypeFamilies
 
 import qualified Cohabr.Db.Tables.Post as P
+import qualified Cohabr.Db.Tables.PostVersion as PV
 
 selectKnownPostsQ :: [Int] -> Select (Field SqlInt4)
 selectKnownPostsQ candidates = proc () -> do
@@ -20,3 +23,18 @@ selectKnownPostsQ candidates = proc () -> do
 
 selectMissingPosts :: [Int] -> PGS.Connection -> IO [Int]
 selectMissingPosts candidates conn = (candidates \\) <$> runSelect conn (selectKnownPostsQ candidates)
+
+selectPostWVersionByHabrIdQ :: Int -> Select (P.Post O, PV.PostVersion O)
+selectPostWVersionByHabrIdQ habrId = proc () -> do
+  post <- selectTable P.postsTable -< ()
+  restrict -< P.sourceId post .== pgInt4 habrId
+  postVersion <- selectTable PV.postsVersionsTable -< ()
+  restrict -< PV.versionId postVersion .== P.currentVersion post
+  returnA -< (post, postVersion)
+
+findPostByHabrId :: Int -> PGS.Connection -> IO (Maybe (P.Post H, PV.PostVersion H))
+findPostByHabrId habrId conn = handleList <$> runSelect conn (selectPostWVersionByHabrIdQ habrId)
+  where
+    handleList [] = Nothing
+    handleList [pair] = Just pair
+    handleList _ = error $ "Multiple posts for habr ID " <> show habrId
