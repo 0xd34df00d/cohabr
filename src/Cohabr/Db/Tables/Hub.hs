914c170a2e19bc5d93ad59590ad319a9baf28b35
0xd34df00d <0xd34df00d@gmail.com>
2019-06-08 14:44:31 -0400

    Use Data.Text in Hub definition

--- src/Cohabr/Db/Tables/Hub.hs
+++ src/Cohabr/Db/Tables/Hub.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE TemplateHaskell, FlexibleInstances, FlexibleContexts, MultiParamTypeClasses, UndecidableInstances, TypeFamilies #-}
 module Cohabr.Db.Tables.Hub where
 import Cohabr.Db.TH
+import qualified Data.Text as T
 
 data Hub f = Hub
-  { hubId   :: TableField f String SqlText NN Req
-  , hubName :: TableField f String SqlText NN Req
+  { hubId   :: TableField f T.Text SqlText NN Req
+  , hubName :: TableField f T.Text SqlText NN Req
   }
 
 $(makeTFAdaptorAndInstance "pHub" ''Hub)
