306e3102d395fceabfe3ddb2d20dcbf2c7873e18
0xd34df00d <0xd34df00d@gmail.com>
2019-06-08 13:04:49 -0400

    Default instance for {Habr,PKey}Id

--- src/Cohabr/Db/HelperTypes.hs
+++ src/Cohabr/Db/HelperTypes.hs
@@ -1,8 +1,15 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances, MultiParamTypeClasses, FlexibleContexts, UndecidableInstances #-}
 
 module Cohabr.Db.HelperTypes where
 
+import Data.Profunctor.Product.Default
 import Opaleye
 
 newtype HabrId = HabrId { habrId :: Int } deriving (Eq, Ord, Show, QueryRunnerColumnDefault PGInt4)
 newtype PKeyId = PKeyId { pkeyId :: Int } deriving (Eq, Ord, Show, QueryRunnerColumnDefault PGInt4)
+
+instance Default Constant Int (Column col) => Default Constant HabrId (Column col) where
+  def = Constant $ constantExplicit def . habrId
+
+instance Default Constant Int (Column col) => Default Constant PKeyId (Column col) where
+  def = Constant $ constantExplicit def . pkeyId
