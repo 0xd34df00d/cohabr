10a30cbc7ef1afd2f591d16d4319797988237957
0xd34df00d <0xd34df00d@gmail.com>
2019-06-12 10:50:12 -0400

    Added a target for one-off database stuff

---
+++ app/DbOneOff/Main.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Main where
+
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
+import qualified Database.PostgreSQL.Simple as PGS
+import Database.Beam
+import Database.Beam.Postgres
+
+import Database.PostgreSQL.Util
+import Cohabr.Db
+import Cohabr.Db.HelperTypes
+
+handleUsers :: PGS.Connection -> [(PKeyId, T.Text)] -> IO ()
+handleUsers conn usersList = do
+  print $ length usersDups
+  mapM_ (reduceDup conn) usersDups
+  where
+    usersDups = filter ((> 1) . length) $ HM.elems $
+                 HM.fromListWith (<>) [ (uname, [uid]) | (uid, uname) <- usersList ]
+
+reduceDup :: PGS.Connection -> [PKeyId] -> IO ()
+reduceDup _ [] = pure ()
+reduceDup conn (keepId : removeIds) = PGS.withTransaction conn $ runBeamPostgresDebug print conn $ do
+  runUpdate $ update (cPosts cohabrDb)
+    (\post -> pAuthor post <-. val_ (Just keepId))
+    (\post -> pAuthor post `in_` removeIdsSql)
+  runUpdate $ update (cComments cohabrDb)
+    (\comm -> cAuthor comm <-. val_ (Just keepId))
+    (\comm -> cAuthor comm `in_` removeIdsSql)
+  runDelete $ delete (cUsers cohabrDb)
+    (\user -> uId user `in_` (val_ <$> removeIds))
+  where
+    removeIdsSql :: forall s. [QGenExpr QValueContext Postgres s (Maybe PKeyId)]
+    removeIdsSql = val_ . Just <$> removeIds
+
+compressUsers :: IO ()
+compressUsers = withConnection $ \conn -> do
+  allUsers <- runBeamPostgres conn $ runSelectReturningList $ select query
+  handleUsers conn allUsers
+  where query = orderBy_ (asc_ . fst) $ fmap (\u -> (uId u, uUsername u)) $ all_ $ cUsers cohabrDb
+
+main :: IO ()
+main = compressUsers
