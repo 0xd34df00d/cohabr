121fa7e323b94633002d34831789020cbe51d3ac
0xd34df00d <0xd34df00d@gmail.com>
2019-06-14 14:13:10 -0400

    Added getPostVersionHubs

--- src/Cohabr/Db/Queries.hs
+++ src/Cohabr/Db/Queries.hs
@@ -21,6 +21,14 @@ findPostByHabrId conn habrId = runBeamPostgresDebug putStrLn conn $ runSelectRet
       postVersion <- filter_ (\postVersion -> pvId postVersion ==. pCurrentVersion post) $ all_ $ cPostsVersions cohabrDb
       pure (post, postVersion)
 
+getPostVersionHubs :: Connection -> PKeyId -> IO [(PostHub, Hub)]
+getPostVersionHubs conn postVersion = runBeamPostgresDebug putStrLn conn $ runSelectReturningList $ select query
+  where
+    query = do
+      postHub <- filter_ (\ph -> phPostVersion ph ==. val_ postVersion) $ all_ $ cPostsHubs cohabrDb
+      hub <- filter_ (\h -> hId h ==. phHub postHub) $ all_ $ cHubs cohabrDb
+      pure (postHub, hub)
+
 findCommentIdByHabrId :: Connection -> HabrId -> IO (Maybe PKeyId)
 findCommentIdByHabrId conn habrId = runBeamPostgresDebug putStrLn conn $ runSelectReturningOne $ select query
   where query = fmap cId $ filter_ (\comm -> cSourceId comm ==. val_ habrId) $ all_ $ cComments cohabrDb
