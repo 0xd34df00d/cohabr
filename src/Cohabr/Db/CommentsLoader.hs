74ad5348ab5c01b3e368c6bb67d8cf211e3f21e0
0xd34df00d <0xd34df00d@gmail.com>
2019-06-17 19:42:13 -0400

    Move comments conversion/loading to a separate module
    It also needs to load the users and avatars, so it's something DB-effectful
    so it does not really belong to Conversions.

---
+++ src/Cohabr/Db/CommentsLoader.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE RankNTypes, FlexibleContexts, ScopedTypeVariables #-}
+{-# LANGUAGE QuasiQuotes, RecordWildCards #-}
+
+module Cohabr.Db.CommentsLoader
+( loadComments
+) where
+
+import qualified Data.IntMap.Strict as IM
+import Control.Monad.Except
+import Data.List.Extra
+import Data.Maybe
+import Data.String.Interpolate
+
+import qualified Habr.Types as HT
+import Habr.Util
+import Cohabr.Db
+import Cohabr.Db.HelperTypes
+import Cohabr.Db.SqlMonad
+import Cohabr.Db.Queries
+import Cohabr.Db.Utils
+
+loadComments :: SqlMonad m => HT.CommentContents -> PKeyId -> m [HT.Comment]
+loadComments defaultContents postId = do
+  storedComments <- getPostComments postId
+  let authors = nubOrd $ mapMaybe cAuthor storedComments
+  avatars <- catMaybes <$> mapM getUserAvatar authors
+  let avatarsByUid = IM.fromList [ (getPKeyId $ uaUser avatar, fromStoredAvatar avatar) | avatar <- avatars ]
+  case runExcept $ fromStoredComments defaultContents avatarsByUid storedComments of
+       Right comments -> pure comments
+       Left err -> throwSql err
+
+fromStoredComments :: forall m. MonadError String m => HT.CommentContents -> IM.IntMap HT.Avatar -> [Comment] -> m [HT.Comment]
+fromStoredComments defaultContents avatars comments = buildCommentsTree <$> mapM convSingle comments
+  where
+    convSingle :: Comment -> m HT.Comment
+    convSingle Comment { .. } = do
+      parentId <- getParentId cParent
+      let commentId = getHabrId cSourceId
+      let avatar = fromMaybe HT.DefaultAvatar $ cAuthor >>= (`IM.lookup` avatars) . getPKeyId
+      let contents
+            | Just text <- cText = HT.CommentExisting
+                { HT.commentText = text
+                , HT.user = fromMaybe user $ HT.UserInfo <$> cUser <*> pure avatar
+                , HT.votes = fromMaybe votes $ HT.Votes <$> cScorePlus <*> cScoreMinus
+                , HT.timestamp = fromMaybe timestamp cDate
+                }
+            | otherwise = HT.CommentDeleted
+      pure HT.Comment { children = [], .. }
+
+    getParentId Nothing = pure 0
+    getParentId (Just parentPKey) | Just val <- IM.lookup (getPKeyId parentPKey) pkey2habrId = pure $ getHabrId val
+                                  | otherwise = throwError [i|Unable to find parent comment with id #{parentPKey}|]
+    pkey2habrId = IM.fromList [ (getPKeyId cId, cSourceId)
+                              | Comment { .. } <- comments
+                              ]
+    HT.CommentExisting { .. } = defaultContents
+
+fromStoredAvatar :: UserAvatar -> HT.Avatar
+fromStoredAvatar = HT.CustomAvatar . uaBigImageUrl
--- src/Cohabr/Db/Conversions.hs
+++ src/Cohabr/Db/Conversions.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE RecordWildCards, OverloadedStrings, QuasiQuotes #-}
-{-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Cohabr.Db.Conversions
 ( makeHubId
@@ -7,20 +7,13 @@ module Cohabr.Db.Conversions
 , fromStoredTag
 , flagToStr
 , strToFlag
-, fromStoredComments
 ) where
 
 import qualified Data.HashMap.Strict as HM
-import qualified Data.IntMap.Strict as IM
 import qualified Data.Text as T
-import Control.Monad.Except
-import Data.Maybe
-import Data.String.Interpolate
 
 import qualified Habr.Types as HT
-import Habr.Util
 import Cohabr.Db
-import Cohabr.Db.HelperTypes
 
 makeHubId :: HT.Hub -> T.Text
 makeHubId HT.Hub { .. } = prefix hubKind <> hubCode
@@ -55,30 +48,3 @@ strToFlagMap :: HM.HashMap T.Text HT.Flag
 strToFlagMap = HM.fromList [ (flagToStr flag, flag)
                            | flag <- [minBound .. maxBound]
                            ]
-
--- This function does not have full info about users,
--- so it will not populate UserInfo besides setting the 'username' field.
-fromStoredComments :: forall m. MonadError String m => HT.CommentContents -> [Comment] -> m [HT.Comment]
-fromStoredComments defaultContents comments = buildCommentsTree <$> mapM convSingle comments
-  where
-    convSingle :: Comment -> m HT.Comment
-    convSingle Comment { .. } = do
-      parentId <- getParentId cParent
-      let commentId = getHabrId cSourceId
-      let contents
-            | Just text <- cText = HT.CommentExisting
-                { HT.commentText = text
-                , HT.user = fromMaybe user $ HT.UserInfo <$> cUser <*> pure HT.DefaultAvatar
-                , HT.votes = fromMaybe votes $ HT.Votes <$> cScorePlus <*> cScoreMinus
-                , HT.timestamp = fromMaybe timestamp cDate
-                }
-            | otherwise = HT.CommentDeleted
-      pure HT.Comment { children = [], .. }
-
-    getParentId Nothing = pure 0
-    getParentId (Just parentPKey) | Just val <- IM.lookup (getPKeyId parentPKey) pkey2habrId = pure $ getHabrId val
-                                  | otherwise = throwError [i|Unable to find parent comment with id #{parentPKey}|]
-    pkey2habrId = IM.fromList [ (getPKeyId cId, cSourceId)
-                              | Comment { .. } <- comments
-                              ]
-    HT.CommentExisting { .. } = defaultContents
