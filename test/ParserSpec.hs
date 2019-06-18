40fe844e6cf6b3f977a26fb98ca3712060fb9b37
0xd34df00d <0xd34df00d@gmail.com>
2019-06-18 12:30:36 -0400

    Added a stub ParserSpec

---
+++ test/ParserSpec.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module ParserSpec(spec) where
+
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import Control.Monad.Except
+import Control.Monad.Extra
+import Control.Monad.Reader
+import Data.Either
+import Data.String.Interpolate
+import Data.Time.LocalTime
+import Network.HTTP.Conduit
+import Test.Hspec
+import Text.HTML.DOM(parseLBS)
+import Text.XML.Cursor(fromDocument)
+import System.Directory.Extra
+
+import Habr.Parser
+
+spec :: Spec
+spec = beforeAll_ fetchPages $
+  describe "Habr parser" $
+    it "parses a page successfully" $ do
+      now <- liftIO $ zonedTimeToLocalTime <$> getZonedTime
+      postPage <- liftIO $ LBS.readFile $ pathForId 203820
+      let root = fromDocument $ parseLBS postPage
+      let parseResult = runExcept $ runReaderT ((,) <$> parsePost root <*> parseComments root) ParseContext { currentTime = now }
+      parseResult `shouldSatisfy` isRight
+
+fetchPages :: IO ()
+fetchPages = do
+  createDirectoryIfMissing True "test/testpages"
+  forM_ ids $ \pgId -> do
+    let pgPath = pathForId pgId
+    unlessM (doesPathExist pgPath) $ do
+      putStrLn [i|Fetching #{pgId}...|]
+      pgContents <- simpleHttp [i|https://habr.com/ru/post/#{pgId}/|]
+      LBS.writeFile pgPath pgContents
+  where ids = [203820]
+
+pathForId :: Int -> FilePath
+pathForId pgId = [i|test/testpages/#{pgId}|]
