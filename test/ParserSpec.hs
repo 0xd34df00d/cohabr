4dc9afe987abef002f625d62e1022e58e7f49dad
0xd34df00d <0xd34df00d@gmail.com>
2019-06-18 12:52:41 -0400

    Make sure title and body also get parsed

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,13 +1,15 @@
-{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes, OverloadedStrings #-}
 
 module ParserSpec(spec) where
 
 import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Text as T
 import Control.Monad.Except
 import Control.Monad.Extra
 import Control.Monad.Reader
 import Data.Either
-import Data.String.Interpolate
+import Data.List
+import Data.String.Interpolate.IsString
 import Data.Time.LocalTime
 import Network.HTTP.Conduit
 import Test.Hspec
@@ -16,15 +18,28 @@ import Text.XML.Cursor(Cursor, fromDocument)
 import System.Directory.Extra
 
 import Habr.Parser
+import Habr.Types
 
 spec :: Spec
 spec = beforeAll_ fetchPages $
-  describe "Habr parser" $
+  describe "Basic page parsing" $ do
     it "parses a page successfully" $ do
-      now <- liftIO $ zonedTimeToLocalTime <$> getZonedTime
+      now <- zonedTimeToLocalTime <$> getZonedTime
       root <- rootElem 203820
       let parseResult = runExcept $ runReaderT ((,) <$> parsePost root <*> parseComments root) ParseContext { currentTime = now }
       parseResult `shouldSatisfy` isRight
+    it "parses the post metainformation correctly" $ do
+      post <- getParsedPost 203820
+      sort (hubs post) `shouldBe` sort [ Hub "silverlight" "Silverlight" NormalHub
+                                       , Hub "net" ".NET" NormalHub
+                                       , Hub "csharp" "C#" NormalHub
+                                       ]
+      sort (flags post) `shouldBe` sort [Translation, Tutorial, Recovery]
+    it "parses the title and body correctly" $ do
+      post <- getParsedPost 203820
+      title post `shouldBe` "WCF RIA Services. Начало. Часть 1"
+      body post `shouldSatisfy` T.isPrefixOf [i|<a href="http://habrahabr.ru/post/203820/">WCF RIA Services.|]
+      body post `shouldSatisfy` T.isSuffixOf [i|<a href="https://github.com/struggleendlessly/Silverlight_WCF_RIA/tree/Part1">Github</a>|]
 
 fetchPages :: IO ()
 fetchPages = do
@@ -40,5 +55,13 @@ fetchPages = do
 pathForId :: Int -> FilePath
 pathForId pgId = [i|test/testpages/#{pgId}|]
 
-rootElem :: MonadIO m => Int -> m Cursor
-rootElem pgId = liftIO $ fromDocument . parseLBS <$> LBS.readFile (pathForId pgId)
+rootElem :: Int -> IO Cursor
+rootElem pgId = fromDocument . parseLBS <$> LBS.readFile (pathForId pgId)
+
+getParsedPost :: Int -> IO Post
+getParsedPost pgId = do
+  now <- zonedTimeToLocalTime <$> getZonedTime
+  root <- rootElem pgId
+  case runExcept $ runReaderT (parsePost root) ParseContext { currentTime = now } of
+    Left err -> error [i|Unable to parse post: #{err}|]
+    Right post -> pure post
