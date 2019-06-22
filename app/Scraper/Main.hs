1aa1e4f6cdadb5e61ab6993417d9d05f76a45a6a
0xd34df00d <0xd34df00d@gmail.com>
2019-06-22 18:52:03 -0400

    Finally doing some scraping!

--- app/Scraper/Main.hs
+++ app/Scraper/Main.hs
@@ -1,17 +1,22 @@
 {-# LANGUAGE NoMonomorphismRestriction, FlexibleContexts, RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings, BangPatterns #-}
 
 module Main where
 
+import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Monad.Except
 import Control.Monad.Reader
+import Data.Time.Clock.POSIX
 import Data.Time.LocalTime
-import Network.HTTP.Conduit
+import Data.Proxy
+import Network.HTTP.Conduit hiding(Proxy)
 import Text.HTML.DOM(parseLBS)
 import Text.XML.Cursor(fromDocument)
 import Time.Repeatedly
+import System.Environment
 import System.IO
+import System.Remote.Monitoring
 
 import Database.PostgreSQL.Util
 import Cohabr.Db
@@ -20,6 +25,7 @@ import Cohabr.Db.HelperTypes
 import Cohabr.Db.Queries
 import Cohabr.Db.SqlMonad
 import Cohabr.Db.Updates
+import Cohabr.Metrics
 import Habr.Parser
 import Habr.RSS
 import Habr.Util
@@ -30,22 +36,52 @@ runSqlMonad act = withConnection $ \c -> runReaderT act SqlEnv { conn = c, stmtL
     logger LogSqlStmt _ = pure ()
     logger _ msg = liftIO $ putStrLn msg
 
-refetchPost :: PostHabrId -> IO ()
-refetchPost habrPostId = do
+time :: MonadIO m => m a -> m (Double, a)
+time act = do
+  start <- liftIO $ realToFrac <$> getPOSIXTime
+  result <- act
+  end <- liftIO $ realToFrac <$> getPOSIXTime
+  let !delta = end - start
+  pure (delta, result)
+
+timed :: forall a m name. (MonadIO m, KnownSymbol name) => MetricsStore -> Metric Distribution name -> m a -> m a
+timed store metric act = do
+  (t, res) <- time act
+  liftIO $ putStrLn $ "Done " <> symbolVal (Proxy :: Proxy name) <> " in " <> show (t * 1000)
+  liftIO $ track store metric $ t * 1000
+  pure res
+
+timedAvg :: forall a m name. (MonadIO m, KnownSymbol name) => MetricsStore -> Metric Distribution name -> Int -> m a -> m a
+timedAvg store metric len act = do
+  (t, res) <- time act
+  liftIO $ putStrLn $ "Done " <> symbolVal (Proxy :: Proxy name) <> "/" <> show len <> " in " <> show (t * 1000)
+  liftIO $ track store metric $ t * 1000 / if len == 0 then 1 else fromIntegral len
+  pure res
+
+refetchPost :: MetricsStore -> PostHabrId -> IO ()
+refetchPost metrics habrPostId = do
+  putStrLn $ "fetching post " <> show habrPostId
   now <- zonedTimeToLocalTime <$> getZonedTime
-  postPage <- simpleHttp $ urlForPostId $ getHabrId habrPostId
+  postPage <- timed metrics PageFetchTime $ simpleHttp $ urlForPostId $ getHabrId habrPostId
   let root = fromDocument $ parseLBS postPage
   let parseResult = runExcept $ runReaderT ((,) <$> parsePost root <*> parseComments root) ParseContext { currentTime = now }
   case parseResult of
-    Left errs -> hPutStr stderr $ unlines errs
+    Left errs -> do
+      hPutStr stderr $ unlines errs
+      error $ show errs
     Right (post, comments) -> do
-      maybeStoredInfo <- runSqlMonad $ getStoredPostInfo habrPostId
+      putStrLn "fetched!"
+      maybeStoredInfo <- timed metrics StoredPostInfoRetrievalTime $ runSqlMonad $ getStoredPostInfo habrPostId
       case maybeStoredInfo of
-        Nothing -> runSqlMonad $ do
-          dbId <- insertPost habrPostId post
-          insertCommentTree dbId comments
-        Just storedInfo -> runSqlMonad $
+        Nothing -> timed metrics TotalInsertTime $ runSqlMonad $ do
+          liftIO $ putStrLn "inserting new one"
+          dbId <- timed metrics PostInsertTime $ insertPost habrPostId post
+          timedAvg metrics PerCommentInsertTime (length comments) $ insertCommentTree dbId comments
+        Just storedInfo -> runSqlMonad $ do
+          liftIO $ putStrLn "updating"
           updatePost $ postUpdateActions storedInfo post
+          updateComments $ commentsUpdatesActions storedInfo comments
+  putStrLn $ "done processing " <> show habrPostId
 
 pollRSS :: IO ()
 pollRSS = do
@@ -55,9 +91,19 @@ pollRSS = do
     Nothing -> undefined
     Just ids -> do
       recs <- runSqlMonad $ selectMissingPosts $ HabrId <$> ids
-      mapM_ refetchPost [head recs]
+      mapM_ (refetchPost undefined) [head recs]
 
 main :: IO ()
 main = do
+  ekgServer <- forkServer "localhost" 8000
+{-
   rssPollHandle <- asyncRepeatedly (1 / 60) pollRSS
   wait rssPollHandle
+  -}
+  [filename] <- getArgs
+  idsStrs <- take 100 . lines <$> readFile filename
+  let ids = read <$> idsStrs
+  withMetricsStore ekgServer $ \metrics ->
+    forM_ ids $ \habrId -> do
+      refetchPost metrics $ HabrId habrId
+      threadDelay 100000
