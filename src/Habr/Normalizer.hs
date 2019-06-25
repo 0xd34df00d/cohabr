7028d21495fa3365884ac1a2747ad18aafbf1403
0xd34df00d <0xd34df00d@gmail.com>
2019-06-25 12:02:34 -0400

    Unicode normalization isn't really necessary anymore

--- src/Habr/Normalizer.hs
+++ src/Habr/Normalizer.hs
@@ -3,18 +3,10 @@
 module Habr.Normalizer where
 
 import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
-import qualified Data.Text.Lazy.Builder as TLB
 import Data.Data
 import Data.Generics.Uniplate.Data
-import HTMLEntities.Decoder
 
 import Habr.Types
-
-normalizeText :: Data from => from -> from
-normalizeText = transformBi f
-  where
-    f = TL.toStrict . TLB.toLazyText . htmlEncodedText
 
 normalizeUrls :: Data from => URL -> from -> from
 normalizeUrls (URL base) = transformBi f
