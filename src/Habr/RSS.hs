59041beef449a8e2851e741346188b9cbd344c4c
0xd34df00d <0xd34df00d@gmail.com>
2019-06-25 16:34:25 -0400

    Added a function to extract last comment date from post RSS

--- src/Habr/RSS.hs
+++ src/Habr/RSS.hs
@@ -2,24 +2,27 @@
 
 module Habr.RSS
 ( recentArticles
+, lastCommentDate
 ) where
 
 import qualified Data.ByteString.Lazy.Char8 as BSL
+import qualified Data.Text as T
+import Control.Monad
 import Data.Maybe
+import Data.Time.Format
+import Data.Time.LocalTime
 import Text.Feed.Import
 import Text.Feed.Types
 import Text.RSS.Syntax
+import Safe.Foldable
 
 import Habr.Internal.Util
 
 recentArticles :: BSL.ByteString -> Maybe [Int]
-recentArticles str = extractArticles <$> parseFeedSource str
-
-extractArticles :: Feed -> [Int]
-extractArticles (RSSFeed RSS { .. }) = extractRssChannel rssChannel
-extractArticles (AtomFeed _) = error "Atom feeds are unsupported"
-extractArticles (RSS1Feed _) = error "RSS1 feeds are unsupported"
-extractArticles (XMLFeed _) = []
+recentArticles str = extract <$> parseFeedSource str
+  where
+    extract (RSSFeed RSS { .. }) = extractRssChannel rssChannel
+    extract _ = error "Unsupported feed format"
 
 extractRssChannel :: RSSChannel -> [Int]
 extractRssChannel RSSChannel { .. } = mapMaybe handleItem rssItems
@@ -30,3 +33,13 @@ extractRssChannel RSSChannel { .. } = mapMaybe handleItem rssItems
       let (_:postId:_) = reverseLinkParts $ rssGuidValue guid
       let Right val = readInt postId
       pure val
+
+lastCommentDate :: BSL.ByteString -> Maybe LocalTime
+lastCommentDate str = parseFeedSource str >>= extract
+  where
+    extract (RSSFeed RSS { .. }) = extractDate rssChannel
+    extract _ = error "Unsupported feed format"
+
+extractDate :: RSSChannel -> Maybe LocalTime
+extractDate RSSChannel { .. } = maximumMay =<<
+  mapM (rssItemPubDate >=> parseTimeM True defaultTimeLocale rfc822DateFormat . T.unpack) rssItems
