70f4979c29ce3875679d954be63fe7556f669785
0xd34df00d <0xd34df00d@gmail.com>
2019-08-17 16:45:48 -0400

    Less code duplication

--- src/Cohabr/Fetch.hs
+++ src/Cohabr/Fetch.hs
@@ -67,9 +67,8 @@ type MetricableSqlMonadRunner = forall a. (forall r m. MetricableSqlMonad r m =>
 
 httpExHandler :: MetricableSqlMonad r m => PostHabrId -> BS.ByteString -> HttpException -> m ()
 httpExHandler habrPostId marker (HttpExceptionRequest _ (StatusCodeException resp respContents))
-  | statusCode (responseStatus resp) == 403
-  , marker `BS.isInfixOf` respContents      = track DeniedPagesCount >> writeLog LogWarn ("Post is unavailable: " <> show habrPostId)
-  | statusCode (responseStatus resp) == 404 = track DeniedPagesCount >> writeLog LogWarn ("Post is unavailable: " <> show habrPostId)
+  | (sc == 403 && marker `BS.isInfixOf` respContents) || sc == 404 = track DeniedPagesCount >> writeLog LogInfo ("Post is unavailable: " <> show habrPostId)
+  where sc = statusCode $ responseStatus resp
 httpExHandler habrPostId _ (HttpExceptionRequest _ ResponseTimeout) = track TimeoutHttpCount >> writeLog LogError ("Request timeout: " <> show habrPostId)
 httpExHandler _ _ ex = throwM ex
 
